6e91c5a899486427b3176411f9fade65dac2d408
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-09-15 05:31:18 -0500

    Add files via upload

---
+++ jlink7.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+#
+# Obfuscate by @CesarHackGray
+# Creditos para @CesarHackGray
+# Servicio de ayuda https://t.me/CesarGray
+# Channel: https://www.youtube.com/channel/UCjs0N8PbEo-se0r_4O_svNQ
+#
+#
+z="
+";dz='ink]';MBz='ed.t';KBz='obfu';Lz='und ';Pz=' "Cr';Jz='-for';uz='do';jz='yell';Oz='p 1';QBz='n)';Az='chmo';BBz='read';az='Redi';FBz=' $li';Sz='"';Cz='7 jl';Vz=' "Co';GBz='nk i';Rz='d by';Uz='n';kz='ow';Kz='egro';gz='  -[';Ez='.sh';rz='rls';HBz='Y)';fz=' "  ';mz='figl';Yz='blue';Hz='sett';CBz=' lin';bz='rect';tz='e :';xz='s [Y';Qz='eate';RBz='esac';iz='nk7]';IBz='cat ';Fz='echo';EBz='case';Gz=' ""';ez='-"';Nz='slee';wz='"Url';OBz='exit';qz='badu';ABz=' "';vz=' -n ';pz='k7';Mz='red';SBz='done';Dz='ink7';oz='-Lin';Wz='mmun';hz='J-Li';Bz='d 77';LBz='scat';lz=' "*"';NBz='xt';Iz='erm ';Tz='gree';Zz=' "-[';DBz='k';PBz=';;';yz='/n]:';nz='et J';Xz='ity"';sz='whil';JBz='url_';cz=' - L';
+eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$Mz$z$Nz$Oz$z$Fz$Pz$Qz$Rz$Sz$z$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$Tz$Uz$z$Fz$Vz$Wz$Xz$z$Nz$Oz$z$Hz$Iz$Jz$Kz$Lz$Yz$z$Fz$Gz$z$Fz$Zz$az$bz$cz$dz$ez$z$Fz$fz$gz$hz$iz$ez$z$Fz$Gz$z$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$jz$kz$z$Fz$lz$z$mz$nz$oz$pz$z$Fz$lz$z$Fz$Gz$z$qz$rz$z$Nz$Oz$z$Fz$Gz$z$sz$tz$z$uz$z$Fz$vz$wz$xz$yz$ABz$z$BBz$CBz$DBz$z$EBz$FBz$GBz$Uz$z$HBz$z$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$Tz$Uz$z$IBz$JBz$KBz$LBz$MBz$NBz$z$OBz$z$PBz$z$QBz$z$OBz$z$PBz$z$RBz$z$SBz"
